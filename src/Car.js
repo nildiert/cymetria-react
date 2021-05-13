9c0bc16e66d388c58a9f465969ef9a9130a4076c
nildiert <niljordan23@gmail.com>
2021-05-13 13:27:31 -0500

    Added Car class

---
+++ src/Car.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Car extends React.Component {
+    render() {
+        return <h2>Hi, I am a Car!</h2>
+    }
+}
+
+// function Car() {
+//     return <h2>Hi, I am a Car!</h2>
+// }
+
+export default Car;
+
